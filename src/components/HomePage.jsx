1d0c541f22d6543107c08872814dd7fdca4d9ec6
Niraj <port.iamniraj@gmail.com>
2026-01-02 21:23:47 +0530

    logo issue

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -4,12 +4,14 @@ import "./Homepage.css";
 import fire from "/assets/fire.png";
 import timerBomb from "/assets/timer-bomb.png";
 
+import gtfIcon from "/assets/gtf.png";
+
 export default function HomePage({ setStartGame, score, setGameType }) {
     return <div className="homepage-wrapper">
         <div className="backdrop-bg">
             <div className="main-page-box">
                 <div className="game-title">
-                    <img src="/assets/gtf.png" alt="Guess The Flag" />
+                    <img src={gtfIcon} alt="Guess The Flag" />
                 </div>
 
                 <div className="game-choice">
